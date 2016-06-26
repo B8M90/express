a0055f21edc486b03a99722f7239d050bb7795c2
Hiroto Nakamoto <nakamoto787@docomo.ne.jp>
2016-06-26 02:19:21 +0000

    Initial commit

--- app.rb
+++ app.rb
@@ -1,3 +1,34 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+get '/' do
+    erb :form
+end
+
+get '/list' do
+ uri = URI("http://express.heartrails.com/api/json")
+ uri.query = URI.encode_www_form({ method:"getStations", x: params[:x], y: params[:y] })
+ res = Net::HTTP.get_response(uri)
+ json = JSON.parse(res.body)
+ @stations = json["response"]["station"]
+ erb :list
+end
+
+get '/api/station' do
+  uri = URI("http://express.heartrails.com/api/json")
+  uri.query = URI.encode_www_form({ method: "getStations", line: params[:line], name: params[:name]})
+  res = Net::HTTP.get_response(uri)
+  json = JSON.parse(res.body)
+ if json["response"]["error"]
+  response = {error: "No station"}
+ else
+  response = {
+ next: json["response"]["station"][0]["next"]
+}
+ end
+json response
+end
